d059a1dcf0adfd4d37d9f7196a9fd94c38745ccf
Oswin florian Bult <obult@f0r5s7.codam.nl>
2021-11-29 15:50:52 +0100

    solve min3A and psuedo for min5A

--- minsteps/solve3.c
+++ minsteps/solve3.c
@@ -1,11 +1,13 @@
 #include "push_swap.h"
 
-void	min_solve_three()
-// if top is == 2 rotate a
-// if second is == 2 rev rotate a
-
-// now if needed swap top 2
-
+void	min_solve_three(t_sort_data data)
+{
+	if ((*data.top_a)->index == 2)
+		ft_rotate(data.top_a, "ra");
+	else if ((*data.top_a)->next->index == 2)
+		ft_rev_rotate(data.top_a, "rra");
+	ft_solve_2a(data);
+}
 
 void	min_solve_five()
 {
@@ -14,3 +16,9 @@ void	min_solve_five()
 	can we swap?
 	
 }
+
+// move closest 4 or 5 to stack b
+// move second closest quickest pos to stack b
+// solve 3 on A
+// move back both of them from B
+// solve 2a
