394cc36c2e83874c050832af786b2d7260f8ea1c
Oswin florian Bult <obult@f1r4s20.codam.nl>
2021-12-05 19:55:45 +0100

    linux added for test.sh

--- test.sh
+++ test.sh
@@ -6,3 +6,10 @@ ARG="$(ruby -e "puts (0..99).to_a.shuffle.join(' ')")"
 ./push_swap $ARG | ./checker_Mac $ARG && ./push_swap $ARG | wc -l
 # echo $ARG
 done
+
+# for i in $(seq $1);
+# do
+# ARG="$(ruby -e "puts (0..99).to_a.shuffle.join(' ')")"
+# ./push_swap $ARG | ./checker_linux $ARG && ./push_swap $ARG | wc -l
+# # echo $ARG
+# done
