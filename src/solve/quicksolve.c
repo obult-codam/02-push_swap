7dc378a2d998ba52b3a47565893f2461d99cc390
Oswin Bult <oswin.bult@gmail.com>
2021-12-10 20:09:53 +0100

    added start of function which will push 5 from Bstack to A and sort them in fewwer steps

--- src/solve/quicksolve.c
+++ src/solve/quicksolve.c
@@ -1,7 +1,7 @@
 #include "push_swap.h"
 #include "libft.h"
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> 
 
 void	ft_hustle(t_sort_data data)
 {
@@ -56,7 +56,6 @@ void	ft_solve_on_b(t_sort_data data)
 {
 	int	r;
 	
-	write(1, "start\n", 6);
 	while (data.size)
 	{
 		if (data.size > 1)
@@ -71,5 +70,38 @@ void	ft_solve_on_b(t_sort_data data)
 			data.size--;
 		}
 	}
-	write(1, "end\n", 4);
+}
+
+int		push_one_a(int range_start, int range_end, t_sort_data data)
+{
+	int	i;
+
+	i = 0;
+	while (i >= 0)
+	{
+		if (ft_offset_index(*data.top_b, -1 * i) <= range_end && 
+				ft_offset_index(*data.top_b, -1 * i) >= range_start)
+		{
+			ft_revr_x(data.top_b, "rrb", i);
+			ft_push(data.top_b, data.top_a, "pa");
+			if ((*data.top_a)->index == range_end)
+				return (1);
+			return (0);
+		}
+		else if (ft_offset_index(*data.top_b, i) <= range_end && 
+			ft_offset_index(*data.top_b, i) >= range_start)
+		{
+			ft_rotate_x(data.top_b, "rb", i);
+			ft_push(data.top_b, data.top_a, "pa");
+			if ((*data.top_a)->index == range_end)
+				return (1);
+			return (0);
+		}
+		i++;
+	}
+}
+
+void	ft_fast_five_b (t_sort_data data)
+{
+	if (push_one_a(4, 5, data)) // 4 and five are placeholders need to use get_min to calc them!
 }
