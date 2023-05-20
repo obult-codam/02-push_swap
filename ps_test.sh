b9f06be49b26a8384c5a7130337ed1acaac2c14e
Oswin Bult <obult@student.codam.nl>
2023-05-20 22:57:06 +0200

    updated test

--- ps_test.sh
+++ ps_test.sh
@@ -3,7 +3,7 @@
 CHECKER=./checker_Mac
 
 # extremeness, how many tests are done for checking amount of operations
-XTN=25
+XTN=1
 
 orange() {
     local orange_color='\033[38;5;214m'  # ANSI escape code for orange color
@@ -11,7 +11,6 @@ orange() {
 
     echo -e "${orange_color}$1${reset_color}"
 }
-
 
 # Function to print OK in green
 print_ok() {
@@ -33,9 +32,10 @@ err_test() {
 	print_ok
 }
 
-no_err_test() {
-	RES=$(./push_swap $1 2>&1)
-	if [ "$RES" == "Error" ]; then
+# base test give "<number array>" as input
+base_test() {
+	RES=$(./push_swap $1 | $CHECKER $1)
+	if [ "$RES" != "OK" ]; then
 		print_ko "$1"
 		exit 1
 	fi
@@ -43,14 +43,13 @@ no_err_test() {
 }
 
 # Test loop for 10 times
-# args 
 do_tests() {
 
 	for ((i=1; i<=$1; i++)); do
 
 		ARG="$(ruby -e "puts (1..$2).to_a.shuffle.join(' ')")"
 		RES=$(./push_swap $ARG | $CHECKER $ARG)
-		if [ $RES == "KO" ]; then
+		if [ "$RES" != "OK" ]; then
 			print_ko "$ARG"
 			exit 1
 		fi
@@ -64,7 +63,6 @@ operations_test() {
 	else
 		LOOPS=$2
 	fi
-	echo "123: " $1 ", " $LOOPS ", " $#
 	MAX=0
 	for ((k=1; k<=$LOOPS; k++)); do
 		ARG="$(ruby -e "puts (1..$1).to_a.shuffle.join(' ')")"
@@ -73,7 +71,7 @@ operations_test() {
 			MAX=$RES
 		fi
 	done
-	echo "max operations of $LOOPS tries: $MAX"
+	echo "max operations for $1 at $LOOPS tries: $MAX"
 }
 
 # Start of testing script
@@ -83,12 +81,12 @@ for ((j=1; j<=6; j++)); do
 	operations_test $j $XTN
 done
 
-echo 
+# 100
 orange "for 100 numbers"
 do_tests 5 100
 operations_test 100 $XTN
 
-echo
+# 500
 orange "for 500 numbers"
 do_tests 5 500
 operations_test 500 $XTN
@@ -114,20 +112,17 @@ err_test "+0 -0"
 
 # perfectly fine input
 orange "perfectly fine input"
-no_err_test "-1 22 3"
+base_test "-1 22 3"
 
+# hand sorting impossible? try 10.000 args
+ARG="$(ruby -e "puts (1..10000).to_a.shuffle.join(' ')")"
+orange "now we will test your program with 10000 arguments"
+# precision
+RES=$(./push_swap $ARG | $CHECKER $ARG)
+echo "result: $RES"
+# operations
+operations_test 10000
 
-
-# # hand sorting impossible, try 10.000 args
-# echo 
-# ARG="$(ruby -e "puts (1..10000).to_a.shuffle.join(' ')")"
-# orange "now we will test your program with 10000 arguments"
-# # precision
-# RES=$(./push_swap $ARG | $CHECKER $ARG)
-# echo "result: $RES"
-# # operations
-# operations_test 10000
-
-# # time
-# echo -n "time: "
-# time ./push_swap $ARG > /dev/null
+# time
+echo -n "time: "
+time ./push_swap $ARG > /dev/null
