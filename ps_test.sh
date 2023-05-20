d779e39d775447ccff63fee427b06069b7e4ac79
Oswin Bult <obult@student.codam.nl>
2023-05-20 23:30:36 +0200

    added evaluate

--- ps_test.sh
+++ ps_test.sh
@@ -19,17 +19,23 @@ print_ok() {
 
 # Function to print KO in red and failed numbers
 print_ko() {
-  echo -e "\033[31mKO\033[0m"
-  echo "Failed numbers: $1"
+  echo $2 -e "[\033[31mKO\033[0m] "
+  echo "Failed numbers: $1" >> "$LOGFILE"
 }
 
 err_test() {
 	RES=$(./push_swap $1 2>&1)
 	if [ "$RES" != "Error" ]; then
 		print_ko "$1"
-		exit 1
 	fi
 	print_ok $2
+}
+
+evaluate() {
+	if [ "$1" != "OK" ]; then
+		print_ko "$2" $3
+	fi
+	print_ok $3
 }
 
 # base test give "<number array>" as input add -n for no newline
@@ -37,7 +43,6 @@ base_test() {
 	RES=$(./push_swap $1 | $CHECKER $1)
 	if [ "$RES" != "OK" ]; then
 		print_ko "$1"
-		exit 1
 	fi
 	print_ok $2
 }
@@ -51,7 +56,6 @@ do_tests() {
 		RES=$(./push_swap $ARG | $CHECKER $ARG)
 		if [ "$RES" != "OK" ]; then
 			print_ko "$ARG"
-			exit 1
 		elif [ i != $1 ]; then
 			print_ok -n
 		fi
@@ -59,7 +63,7 @@ do_tests() {
 	print_ok $3
 }
 
-operations_test() {
+operations_benchmark() {
 	if [ $# -eq 1 ]; then
 		LOOPS=1
 	else
@@ -80,18 +84,18 @@ operations_test() {
 for ((j=1; j<=6; j++)); do
 	orange "tests for $j numbers"
 	do_tests 5 $j
-	operations_test $j $ROUNDS
+	operations_benchmark $j $ROUNDS
 done
 
 # 100
 orange "for 100 numbers"
 do_tests 5 100
-operations_test 100 $ROUNDS
+operations_benchmark 100 $ROUNDS
 
 # 500
 orange "for 500 numbers"
 do_tests 5 500
-operations_test 500 $ROUNDS
+operations_benchmark 500 $ROUNDS
 
 # random tests
 orange "random tests"
@@ -121,9 +125,9 @@ ARG="$(ruby -e "puts (1..10000).to_a.shuffle.join(' ')")"
 orange "now we will test your program with 10000 arguments"
 # precision
 RES=$(./push_swap $ARG | $CHECKER $ARG)
-echo "result: $RES"
+evaluate "$RES" "-10000 args"
 # operations
-operations_test 10000
+operations_benchmark 10000
 
 # time
 echo -n "time: "
